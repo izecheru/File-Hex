e46e38140f1c282f7a2467750e2d3f7103271f88
Ionut <56791024+izecheru@users.noreply.github.com>
2024-06-09 21:05:32 +0300

    Reformated code + tested some stuff

--- File-Hex.cpp
+++ File-Hex.cpp
@@ -7,16 +7,19 @@
 #include <sstream>
 #include <vector>
 
-void toFile(const std::string& inputFilePath, const std::string& outputFilePath) {
+void toFile(const std::string &inputFilePath, const std::string &outputFilePath)
+{
     std::ifstream inputFile(inputFilePath, std::ios::binary | std::ios::in);
     std::ofstream outputFile(outputFilePath, std::ios::binary | std::ios::out);
 
-    if (!inputFile.is_open()) {
+    if (!inputFile.is_open())
+    {
         std::cout << "Error opening input file!" << std::endl;
         return;
     }
 
-    if (!outputFile.is_open()) {
+    if (!outputFile.is_open())
+    {
         std::cout << "Error opening output file!" << std::endl;
         return;
     }
@@ -25,7 +28,8 @@ void toFile(const std::string& inputFilePath, const std::string& outputFilePath)
     inputFile >> std::noskipws >> hexData;
 
     // Process the hex data and convert it to binary
-    for (std::size_t i = 0; i < hexData.length(); i += 2) {
+    for (std::size_t i = 0; i < hexData.length(); i += 2)
+    {
         unsigned int byte;
         std::stringstream ss;
         ss << std::hex << hexData.substr(i, 2);
@@ -37,22 +41,26 @@ void toFile(const std::string& inputFilePath, const std::string& outputFilePath)
     outputFile.close();
 }
 
-void toHex(const std::string& inputFilePath, const std::string& outputFilePath) {
+void toHex(const std::string &inputFilePath, const std::string &outputFilePath)
+{
     std::ifstream inputFile(inputFilePath, std::ios::binary | std::ios::in);
     std::ofstream outputFile(outputFilePath, std::ios::binary | std::ios::out);
 
-    if (!inputFile.is_open()) {
+    if (!inputFile.is_open())
+    {
         printf("Error opening input file!\n");
         return;
     }
 
-    if (!outputFile.is_open()) {
+    if (!outputFile.is_open())
+    {
         printf("Error opening output file!\n");
         return;
     }
 
     char byte;
-    while (inputFile.get(byte)) {
+    while (inputFile.get(byte))
+    {
         outputFile << std::hex << std::setw(2) << std::setfill('0') << (0xff & (int)byte);
     }
 
@@ -60,13 +68,15 @@ void toHex(const std::string& inputFilePath, const std::string& outputFilePath) 
     outputFile.close();
 }
 
-std::string getExtension(std::string fileName) {
+std::string getExtension(std::string fileName)
+{
     std::string result;
 
     return result;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     std::cout << "                                                  | |                \n";
     std::cout << "       ___    ___    _ __   __   __   ___   _ __  | |_    ___   _ __ \n";
     std::cout << "      / __|  / _ \\  | '_ \\  \\ \\ / /  / _ \\ | '__| | __|  / _ \\ | '__|\n";
@@ -74,29 +84,35 @@ int main(int argc, char* argv[]) {
     std::cout << " (_)  \\___|  \\___/  |_| |_|   \\_/    \\___| |_|     \\__|  \\___| |_|   \n";
     std::cout << "                                                                      \n";
 
-
-    switch (argc) {
-    case 5: {
-        //      0       1   2      3        4
-        // ./converter -a -f/-h inputDir outputDir
-        if (strcmp(argv[1], "-a") == 0) {
+    switch (argc)
+    {
+    case 5:
+    {
+        if (strcmp(argv[1], "-a") == 0)
+        {
             std::string dirPath(argv[3]);
             printf("\nInput dir:\t%s", dirPath.c_str());
             std::filesystem::create_directory(argv[4]);
             printf("\nOutput dir:\t%s\n", argv[4]);
             std::string outDir(argv[4]);
-            if (strcmp(argv[2], "-h") == 0) {
-                for (const auto& file : std::filesystem::directory_iterator(dirPath)) {
-                    if (strcmp(file.path().string().c_str(), outDir.c_str()) == 0) {
+            if (strcmp(argv[2], "-h") == 0)
+            {
+                for (const auto &file : std::filesystem::directory_iterator(dirPath))
+                {
+                    if (strcmp(file.path().string().c_str(), outDir.c_str()) == 0)
+                    {
                         continue;
                     }
                     printf("[hexing file]\t%s\n", file.path().string().c_str());
                     toHex(file.path().string(), outDir + "\\" + file.path().stem().string() + file.path().extension().string() + ".bin");
                 }
             }
-            else if (strcmp(argv[2], "-f") == 0) {
-                for (const auto& file : std::filesystem::directory_iterator(dirPath)) {
-                    if (strcmp(file.path().string().c_str(), outDir.c_str()) == 0) {
+            else if (strcmp(argv[2], "-f") == 0)
+            {
+                for (const auto &file : std::filesystem::directory_iterator(dirPath))
+                {
+                    if (strcmp(file.path().string().c_str(), outDir.c_str()) == 0)
+                    {
                         continue;
                     }
                     printf("[converting file]\t%s\n", file.path().string().c_str());
@@ -105,21 +121,25 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-          break;
-    case 4: {
+    break;
+    case 4:
+    {
         // a specific conversion was given
         std::string inputFilePath = argv[2];
         std::string outputFilePath = argv[3];
 
-        if (strcmp(argv[1], "-f") == 0) {
+        if (strcmp(argv[1], "-f") == 0)
+        {
             toFile(inputFilePath, outputFilePath);
         }
-        else if (strcmp(argv[1], "-h") == 0) {
+        else if (strcmp(argv[1], "-h") == 0)
+        {
             toHex(inputFilePath, outputFilePath);
         }
     }
-          break;
-    default: {
+    break;
+    default:
+    {
         printf("\n\nUsage %s ", argv[0]);
         printf("\n\t<-h>\tfor file to hex\n\t<-f>\tfor hex to file (provide file type at the end eg file.jpg)");
         printf("\n\t<-a> <path to input dir> <path to output dir>\tfor all the files in the dir to be converted\n");
---
+++ README.md
@@ -0,0 +1,23 @@
+# File-Hex
+
+Utility to turn file data into hexadecimal representation
+
+## Example
+
+![test picture](screenshot.png?raw=true)
+
+### Usage
+
+This turns a file into hex representation, you can also provide the file extension for output_file
+
+```
+./File-hex <-h> <input_file> <output_file>
+```
+
+Reverting a file (you have to provide the file extension yourself)
+
+```
+./File-hex <-f> <input_file> <output_file>
+```
+
+The whole directory is still a work in progress so it's not tesetd at the moment.
